942e52bd0959778bace8ff0336a1c8bf6c60d046
Kaushal oli <81160647+oliKaushal@users.noreply.github.com>
2026-02-05 12:20:21 +0530

    Create Customers with sales above 20,000.sql

---
+++ Customers with sales above 20,000.sql
@@ -0,0 +1,6 @@
+SELECT c.customer_name, SUM(o.order_amount) AS total_sales
+FROM orders o
+JOIN customers c
+ON o.customer_id = c.customer_id
+GROUP BY c.customer_name
+HAVING SUM(o.order_amount) > 20000;
