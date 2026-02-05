feef6f8ab211788fff811eda87b733af7e641d44
Kaushal oli <81160647+oliKaushal@users.noreply.github.com>
2026-02-05 12:14:19 +0530

    Create customers.sql

---
+++ customers.sql
@@ -0,0 +1,11 @@
+CREATE TABLE customers (
+  customer_id VARCHAR(5),
+  customer_name VARCHAR(50),
+  city VARCHAR(30)
+);
+
+INSERT INTO customers VALUES
+('C101','Rohit Sharma','Delhi'),
+('C102','Anita Verma','Mumbai'),
+('C103','Suresh Kumar','Bangalore'),
+('C104','Neha Singh','Pune');
