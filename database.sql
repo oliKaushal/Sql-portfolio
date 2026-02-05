331e5c5f7e7ae1084d7fec597a8d054352134efd
Kaushal oli <81160647+oliKaushal@users.noreply.github.com>
2026-02-05 12:13:06 +0530

    Create database.sql

---
+++ database.sql
@@ -0,0 +1,9 @@
+CREATE TABLE customers (
+  customer_id VARCHAR(5),
+  name VARCHAR(50),
+  region VARCHAR(20)
+);
+
+INSERT INTO customers VALUES
+('C001','ABC Traders','North'),
+('C002','XYZ Pvt Ltd','West');
